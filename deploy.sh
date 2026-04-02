3c2a51a4f2349194c7ae2bde370e848bfa529947
asnovomlinov <asnovomlinov@MSK-J32J16L720.local>
2026-04-02 12:00:31 +0300

    Final submission: standalone pod, rollout logic, and ready-checks included

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,33 @@
 #!/bin/bash
-
-
-
 set -e
 
-
-
 echo "=== 1. Собираем Docker образ ==="
-
 docker build -t custom-app-image:latest ./app
 
-
-
-echo "=== 2. Применяем настройки (ConfigMap) ==="
-
+echo "=== 2. Применяем настройки ==="
 kubectl apply -f k8s/01-configmap.yaml
 
+echo "=== 2.5 Тестовый Pod  ==="
+kubectl apply -f k8s/01-5-pod.yaml
 
-
-echo "=== 3. Разворачиваем приложение (Deployment & Service) ==="
-
+echo "=== 3. Разворачиваем приложение ==="
 kubectl apply -f k8s/02-app.yaml
 
-
-
 echo "=== 4. Запускаем сборщик логов (DaemonSet) ==="
-
 kubectl apply -f k8s/03-daemonset.yaml
 
-
-
 echo "=== 5. Настраиваем архивацию (CronJob) ==="
-
 kubectl apply -f k8s/04-cronjob.yaml
 
+echo "=== 6. Ожидание готовности ключевых компонентов... ==="
+kubectl wait --for=condition=ready pod/test-app-pod --timeout=60s
 
+kubectl rollout status deployment/my-app
+
+echo "=== 7. Перезапуск при изменении конфига ==="
+kubectl rollout restart deployment my-app
+kubectl rollout status deployment/my-app
 
 echo "=== Готово! === "
+echo "Для проверки API выполни: kubectl port-forward svc/app-service 8080:80"
 
-echo "Проверить поды можно командой: kubectl get pods"
-
-echo "Чтобы пробросить порт и проверить API, выполни:"
-
-echo "kubectl port-forward svc/app-service 8080:80"
